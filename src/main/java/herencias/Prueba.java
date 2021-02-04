1e683522280c32cefcc53c2ee140094e2546cd13
Isaac <jimenezmerino.isaac9871@gmail.com>
2021-02-04 10:37:21 +0100

    Creación de las instancias de las clases en la clase Prueba y añadidos luego a la ArrayList de la clase Padre

--- src/main/java/herencias/Prueba.java
+++ src/main/java/herencias/Prueba.java
@@ -5,12 +5,39 @@
  */
 package herencias;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author isaac
  */
 public class Prueba {
+
     public static void main(String[] args) {
+        //Creación de los objetos
+        Ajedrez juego1 = new Ajedrez(64, "Acorralar el rey enemigo");
+        Ajedrez juego2 = new Ajedrez(64, "Defender tu rey del jugador enemigo");
+
+        Piezas peonesB = new Piezas(8, true, 64, "Derrotar al ejercito enemigo");
+        Piezas peonesN = new Piezas(8, false, 64, "Derrotar al ejercito enemigo");
+
+        Peon ficha1 = new Peon(1, "frontal", 1, true, 64, "Defender a su rey");
+        Peon ficha2 = new Peon(1, "frontal", 1, false, 64, "Derrotar al rey enemigo");
+
+        Caballo caballito1 = new Caballo(4, "En forma de L", 1, true, 64, "Limitar el movimiento de las piezas enemigas");
+        Caballo caballito2 = new Caballo(4, "En forma de L", 1, false, 64, "ponerse en un lugar donde protega a las piezas aliadas");
         
+        //Creación del ArrayList
+        ArrayList<Ajedrez> lista = new ArrayList<>();
+        //Añadir objetos creados anteriormente a la ArrayList
+        lista.add(juego1);
+        lista.add(juego2);
+        lista.add(peonesB);
+        lista.add(peonesN);
+        lista.add(ficha1);
+        lista.add(ficha2);
+        lista.add(caballito1);
+        lista.add(caballito2);
+
     }
 }
